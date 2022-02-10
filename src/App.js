6df35117ddc6b33453a6a7af7b02e71381dfde3b
hazim0001 <hazim_alsayed@hotmail.com>
2022-02-10 22:20:39 +0200

    finished image upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const uploadImage = (files) => {
+    console.log(files[0]);
+    const formData = new FormData();
+    formData.append("file", files[0]);
+    formData.append("upload_preset", "gn1vvhyt");
+    axios
+      .post("https://api.cloudinary.com/v1_1/h465141h/image/upload", formData)
+      .then((res) => {
+        console.log(res);
+      });
+    // setIsFilePicked(true);
+  };
+
+  const handleSubmission = () => {};
+  console.log(selectedFile);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type="file"
+          name="file"
+          onChange={(e) => uploadImage(e.target.files)}
+        />
+        {isFilePicked ? (
+          <div>
+            <p>Filename: {selectedFile.name}</p>
+            <p>Filetype: {selectedFile.type}</p>
+            <p>Size in bytes: {selectedFile.size}</p>
+            <p>
+              lastModifiedDate:{" "}
+              {/* {selectedFile.lastModifiedDate.toLocaleDateString()} */}
+            </p>
+          </div>
+        ) : (
+          <p>Select a file to show details</p>
+        )}
+        <div>
+          <button onClick={handleSubmission}>Submit</button>
+        </div>
+      </div>
     </div>
   );
 }
